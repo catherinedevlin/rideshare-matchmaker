d712b3434c300beb4b28e4b6d6d80a508cff23c3
Catherine Devlin <catherine.devlin@gsa.gov>
2016-07-30 20:35:01 -0400

    Add geocoder

--- rideshare_matchmaker/traveller/models.py
+++ rideshare_matchmaker/traveller/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from geopy.geocoders import Nominatim
+
 
 class Location(models.Model):
     postal_code = models.CharField()
     latitude = models.FloadField()
     longitude = models.FloadField()
+
+    geolocator = Nominatim()
+
+    class geocode(self):
+        point = self.geolocator.geocode({'country': 'USA',
+                                         'postal_code': self.postal_code})
+        (self.latitude, self.longitude) = (point.latitude, point.longitude)
 
 
 class Destination(models.Model):
